5f63ceaa21cff525c0ab2cf62ff6bfdca489c9f8
Trunghoang21 <trunghoang2179@gmail.com>
2025-09-09 03:09:43 +0200

    working on small example

--- labs/lab1/preflow.scala
+++ labs/lab1/preflow.scala
@@ -8,11 +8,14 @@ import scala.concurrent.{Await,ExecutionContext,Future,Promise}
 import scala.concurrent.duration._
 import scala.language.postfixOps
 import scala.io._
+import math.min
+import math.abs
 
 case class Flow(f: Int) // case class for data with parameters
 case class Debug(debug: Boolean)
 case class Control(control:ActorRef)
 case class Source(n: Int)
+
 
 case object Print // case object for singletons for parameterless messages. 
 case object Start
@@ -20,7 +23,7 @@ case object Excess
 case object Maxflow
 case object Sink
 case object Hello
-case object End
+case object GetHeight
 
 
 class Edge(var u: ActorRef, var v: ActorRef, var c: Int) {
@@ -51,14 +54,17 @@ class Node(val index: Int) extends Actor {
 		{if (debug) println(str +s" mess from ${id}")}
 	
 	def start_push : Unit = {
-		for (e <- edge){
-			custom_print("start push to " + e.v)
+		var start_flow = 0;
+		for (ed <- edge){
+			custom_print("start push to " + ed.v)
 			// find repecipent 
-			if (e.u == self){
-				e.f = e.c
-				e.v ! Flow(e.c)
+			if (ed.u == self){
+				ed.f = ed.c
+				start_flow += ed.c
+				ed.v ! Flow(ed.c)
 			}
 		}
+		//println(s"flow from source from beginning ${start_flow} excess preflow from source ${e} ")
 	}
 
 	def enter(func: String): Unit = { if (debug) { println(id + " enters " + func); status } } // helper fuunction for debug
@@ -107,18 +113,73 @@ class Node(val index: Int) extends Actor {
 	}
 
 	case Flow(f:Int) =>{
-		this.e = f
-		custom_print("update the flow to: " + this.e)
-		for (e <- edge){
-			if(e.u == sender && e.v == self){
-				e.f = f
-				println(s"flow from edge ${e.u} to ${e.v} to ${e.f}") 	
+		e += f
+		custom_print("update the flow to: " + e)
+		//for (e <- edge){
+		//	if(e.v == sender || e.u == sender){
+		//		e.f += f
+				//println(s"flow from edge ${e.u} to ${e.v} to ${e.f}") 	
+		//	}
+		//}
+		if(!source && !sink){ self ! Hello }
+		else{
+			if (source){
+				control ! Source(e)  
 			}
-		}  
+		}
+
 	}
 
-	
+	case GetHeight =>{
+		sender ! h // send back the current height. 
+	}
 
+	case Hello =>{
+		if (e > 0 && !sink){
+			var pushSuccessful = false
+			for (ed <- edge if!pushSuccessful ){
+				if (ed.u == self && ed.c > ed.f){
+					implicit val timeout = Timeout(10.seconds)
+					var heightFuture = ed.v ? GetHeight
+					var height = Await.result(heightFuture, timeout.duration).asInstanceOf[Int]
+					if (h > height){
+						var flowToPush = min(e, ed.c - ed.f)
+						e -= flowToPush
+						ed.f += flowToPush
+						//println(s"push from ${index} with flow ${flowToPush}")   
+						ed.v ! Flow(flowToPush)
+						self ! Hello
+						pushSuccessful = true
+					}
+				}
+				else if (ed.v == self && ed.f > 0){
+					implicit val timeout = Timeout(10.seconds)
+					var heightFuture = ed.u ? GetHeight
+					var height = Await.result(heightFuture, timeout.duration).asInstanceOf[Int]
+					if (h > height){
+						var flowToBack = min(e, ed.f)
+						e -= flowToBack
+						//println(s"before update ${ed.f}")
+						ed.f = ed.f - flowToBack
+						//println(s"push back from ${index} with flow ${ed.f}")   
+						ed.u ! Flow(-ed.f)
+						self ! Hello
+						pushSuccessful = true
+					}
+				}
+
+			}
+			if(pushSuccessful){
+				if (e > 0){
+					self ! Hello
+				}
+			}else{
+				relabel
+				self ! Hello
+			}
+		}
+	} 
+	
 	case _		=> {
 		println("" + index + " received an unknown message" + _) }
 
@@ -132,9 +193,7 @@ class Preflow extends Actor
 {
 	var	s	= 0;			/* index of source node.					*/
 	var	t	= 0;			/* index of sink node.					*/
-	var	n	= 0;
-	//var flow_start = 0;
-	//var flow_sink = 0;			/* number of vertices in the graph.				*/
+	var	n	= 0;            /* number of vertices in the graph.				*/
 	var	edge:Array[Edge]	= null	/* edges in the graph.						*/
 	var	node:Array[ActorRef]	= null	/* vertices in the graph.					*/
 	var	ret:ActorRef 		= null	/* Actor to send result to.					*/
@@ -148,7 +207,7 @@ class Preflow extends Actor
 		t = n-1
 		for (u <- node){
 			u ! Control(self)
-			u ! Debug(true)
+			//u ! Debug(true)
 		}	
 			
 		// impl for setting the source, sink
@@ -182,13 +241,34 @@ class Preflow extends Actor
 	case Maxflow => {
 		ret = sender
 
-		node(t) ! Excess	/* ask sink for its excess preflow (which certainly still is zero). */
+		
+		//node(t) ! Excess	/* ask sink for its excess preflow (which certainly still is zero). */
 	}
+	
+	case Source(n:Int) =>{
+		var fromSource = abs(n);
+		implicit val timeout = Timeout(2.seconds)
+		val sinkFuture = node(t) ? Excess
+		val sinkResult = Await.result(sinkFuture, timeout.duration)
+    	val sinkExess = sinkResult match {
+      		case Flow(f) => f
+      		case i: Int => i
+      		case _ => throw new Exception("Unexpected message type for sink excess")
+    	}
+
+		if (fromSource == sinkExess && ret != null){
+			ret ! sinkExess
+		} 
+		
+	}
+		
+	
+
 	}
 }
 
 object main extends App {
-	implicit val t = Timeout(4 seconds);
+	implicit val t = Timeout(30	.seconds);
 
 	val	begin = System.currentTimeMillis()
 	val system = ActorSystem("Main")
@@ -212,9 +292,9 @@ object main extends App {
 
 	for (i <- 0 to n-1)
 		node(i) = system.actorOf(Props(new Node(i)), name = "v" + i)
-
+	// create node
 	edge = new Array[Edge](m)
-
+	// create edges
 	for (i <- 0 to m-1) {
 
 		val u = s.nextInt
@@ -237,12 +317,12 @@ object main extends App {
 	*/
 	val f = Await.result(flow, t.duration)
 
-	println("f = " + f)
+	println(f)
 
 	system.stop(control);
 	system.terminate()
 
 	val	end = System.currentTimeMillis()
 
-	println("t = " + (end - begin) / 1000.0 + " s")
+	//println("t = " + (end - begin) / 1000.0 + " s")
 }
